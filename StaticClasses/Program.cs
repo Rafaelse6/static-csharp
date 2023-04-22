5dd0de74e3ff3d8357564ef12c69adbbf1857601
Rafael Santos <rafa.05125@gmail.com>
2023-04-21 22:10:27 -0300

    Solution in the main function

--- StaticClasses/Program.cs
+++ StaticClasses/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace StaticClasses
+{
+    class Program
+    {
+
+        static double Pi = 3.14;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Circunferencia(raio);
+            double volume = Volume(raio);
+
+            Console.WriteLine("Circunferência : " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+
+        static double Circunferencia(double r)
+        {
+            return 2 * Pi * r;
+        }
+
+        static double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+    }
+}
