cfaa44e6e544a3b6ab4bc40e12aca4698f1ecf3e
Rafael Santos <rafa.05125@gmail.com>
2023-04-22 20:17:12 -0300

    Solution with Calculadora class

--- StaticClasses/Program.cs
+++ StaticClasses/Program.cs
@@ -5,30 +5,23 @@ namespace StaticClasses
 {
     class Program
     {
-
-        static double Pi = 3.14;
-
         static void Main(string[] args)
         {
+
+            Calculadora calc = new Calculadora();
+
+
             Console.Write("Entre o valor do raio: ");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double circ = Circunferencia(raio);
-            double volume = Volume(raio);
+            double circ = calc.Circunferencia(raio);
+
+            double volume = calc.Volume(raio);
 
             Console.WriteLine("Circunferência : " + circ.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Valor de PI: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
         }
 
-        static double Circunferencia(double r)
-        {
-            return 2 * Pi * r;
-        }
-
-        static double Volume(double r)
-        {
-            return 4.0 / 3.0 * Pi * r * r * r;
-        }
     }
 }
